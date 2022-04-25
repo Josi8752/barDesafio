20a313d762b24faf88391d0d3639860d5b29148b
Josiane Silva <josianesouza8752@gmail.com>
2022-04-25 14:11:40 -0300

    End

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,7 +10,7 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("Sexo: ");
 		Bill.gender = sc.next().charAt(0);
 		System.out.print("Quantidade de cervejas: ");
@@ -19,21 +19,20 @@ public class Program {
 		Bill.softDrink = sc.nextInt();
 		System.out.print("Quantidade de espetinhos: ");
 		Bill.barbecue = sc.nextInt();
-		
+
 		System.out.println();
 		System.out.println("RELATÓRIO: ");
-		System.out.printf("Consumo = %.2f\n ",Bill.feeding());
-		
-		if (Bill.cover()== 0) {
+		System.out.printf("Consumo = %.2f\n ", Bill.feeding());
+
+		if (Bill.cover() == 0) {
 			System.out.println("Isento de Couvert ");
+		} else {
+			System.out.printf("Couvert = %.2f\n ", Bill.cover());
 		}
-		else {
-			System.out.printf("Couvert = %.2f\n ",Bill.cover());
-		}
-		
+
 		System.out.printf("Ingresso = %.2f\n ", Bill.ticket());
 		System.out.println();
-		System.out.printf("Valor a pagar = R$ %.2f\n ",Bill.total());
+		System.out.printf("Valor a pagar = R$ %.2f\n ", Bill.total());
 		sc.close();
 	}
 
